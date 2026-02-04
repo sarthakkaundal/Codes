4a077566fadc6ec8a3c66259732e6dfea5797b51
Sarthak <skkaundal9314@gmail.com>
2026-02-04 13:28:04 +0530

    h-index

---
+++ LC274.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int hIndex(int[] citations) {
+        Arrays.sort(citations);
+        int n = citations.length;
+        for(int i=0; i<citations.length; i++){
+            if(citations[i] >= n-i){
+                return n-i;
+            }           
+        }
+        return 0;
+    }
+}
