47f92914160ef69afc8e0d2fe1af2647d4e93317
Sarthak <skkaundal9314@gmail.com>
2026-02-04 13:36:19 +0530

    h-index

--- LC274.java
+++ LC274.java
@@ -1,10 +1,12 @@
-// Approach
-// Sort the citations array in ascending order.
-// Let n be the total number of papers.
-// Traverse the array from left to right.
-// At index i, check if citations[i] ≥ n − i (papers remaining including current).
-// The first index where this holds gives the maximum possible H-index (n − i).
-// If no index satisfies the condition, return 0.
+/*
+Approach
+Sort the citations array in ascending order.
+Let n be the total number of papers.
+Traverse the array from left to right.
+At index i, check if citations[i] ≥ n − i (papers remaining including current).
+The first index where this holds gives the maximum possible H-index (n − i).
+If no index satisfies the condition, return 0.
+*/
 
 class Solution {
     public int hIndex(int[] citations) {
